2b56122ea4eb8c5cd7e84ff3493e3ad4b426e9f5
Jesse Theisen <theiseja@gmail.com>
2020-04-18 00:08:17 -0500

    Adds some notes on functions

--- notes.js
+++ notes.js
@@ -1,2 +1,12 @@
 // just basic notes while learning JS. And some code examples
 
+// *** variables ***
+var
+const
+let
+
+
+// callback function example
+const testFunction = function(callback) {
+    callback('callback test')
+};
